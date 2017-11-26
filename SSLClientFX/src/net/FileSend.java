095b97ffb94834b431f5e3cd2f2019978e10f914
luisbraganca <luisbraganca@outlook.com>
2017-11-26 03:24:33 +0000

    Improved javadoc and added FileTransmission.java for better abstractions

--- SSLClientFX/src/net/FileSend.java
+++ SSLClientFX/src/net/FileSend.java
@@ -5,76 +5,35 @@
  */
 package net;
 
-import javax.net.ssl.SSLSocket;
-import javax.net.ssl.SSLSocketFactory;
 import java.io.*;
 
 /**
  * @author lbsilva
  */
-public class FileSend {
+public class FileSend extends FileTransmission {
 
     /**
-     *
-     */
-    private final static String SERVER_IP = "REPLACE.WITH.SERVER.IP";
-
-    /**
-     * Socket port to use when transfering files
-     */
-    private final static int SERVER_PORT = 6000;
-
-    /**
-     * Buffer size for file transfer
-     */
-    private final static int BUFFER_SIZE = 1024 * 16;
-
-    /**
-     * The file that will be sent
-     */
-    private File fileToSend;
-
-    /**
-     * The sslSocket to be used
-     */
-    private SSLSocket sslSocket;
-
-    /**
-     * If sending was canceled by the user
-     */
-    private boolean canceled;
-
-    /**
-     * Current status of file transfer
-     */
-    private String status;
-
-    /**
-     * A Thread, ready to send a file to a certain IP with an available status property
+     * Sends a file to a certain Id
      *
      * @param path - Path of the file to be sent
      * @param id   - destination id
-     * @throws FileNotFoundException - If there's no file
+     * @throws IOException - Some network failure
      */
     public FileSend(String path, String id) throws IOException {
-        if (path.isEmpty()) {
-            throw new RuntimeException("Empty File Path.");
-        }
-        fileToSend = new File(path);
-        if (!fileToSend.exists()) {
+        super(path);
+        if (!file.exists()) {
             throw new FileNotFoundException("File\n\"" + path + "\"\nnot found.");
         }
-        try {
-            SSLSocketFactory sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-            sslSocket = (SSLSocket) sslSocketFactory.createSocket(SERVER_IP, SERVER_PORT);
-        } catch (IOException ex) {
-            throw new UnreachableHostException("Unreachable Host.");
-        }
-        canceled = false;
-        hanshake(id);
+        handshake(id);
     }
 
-    private void hanshake(String id) throws IOException {
+    /**
+     * Handshake process with the server
+     *
+     * @param id - the id
+     * @throws IOException - Some network failure
+     */
+    private void handshake(String id) throws IOException {
         new Transmission(sslSocket).sendText("SENDER");
         if (!new Transmission(sslSocket).readText().equals("RECEPTOR_ID?"))
             throw new HandshakeException("Handshake error");
@@ -87,57 +46,27 @@ public class FileSend {
         }
     }
 
-    /**
-     * Getter for status
-     *
-     * @return the status of the current transfer
-     */
-    public String getStatus() {
-        return status;
-    }
-
+    @Override
     public void start() {
         FileInputStream fis = null;
         BufferedInputStream bis = null;
         OutputStream os = null;
         try {
-            fis = new FileInputStream(fileToSend);
+            fis = new FileInputStream(file);
             bis = new BufferedInputStream(fis);
             os = sslSocket.getOutputStream();
-            System.out.println("Sending\n" + fileToSend.getAbsolutePath() + "\n(" + fileToSend.length() + " bytes)");
             int count;
             byte[] buffer = new byte[BUFFER_SIZE];
             while (!canceled && (count = bis.read(buffer)) > 0) {
                 os.write(buffer, 0, count);
             }
-            os.flush();
-            if (!canceled) {
-                status = "Sent\n\"" + fileToSend.getAbsolutePath() + "\"\n(" + fileToSend.length() + " bytes)";
-            } else {
-                status = "Canceled";
-            }
+            os.flush(); // It's a bit redundant but just in case
+            status = canceled ? "Canceled" : "Sent\n\"" + file.getAbsolutePath() + "\"\n(" + file.length() + " bytes)";
         } catch (IOException exception) {
             canceled = true;
-            status = exception.getMessage();
+            status = "Either canceled or some error occurred.";
         } finally {
             finish(fis, bis, os, sslSocket);
         }
-    }
-
-
-    /**
-     * Finishes a transformation process by closing all the streams and
-     * deleting the file if it was aborted
-     *
-     * @param closeables the streams
-     */
-    private void finish(Closeable... closeables) {
-        for (Closeable closeable : closeables) {
-            try {
-                closeable.close();
-            } catch (Exception ignored) {
-            }
-        }
-        if (canceled) fileToSend.delete(); // If it was aborted the file might be corrupted
     }
 }
---
+++ SSLClientFX/src/net/FileTransmission.java
@@ -0,0 +1,92 @@
+package net;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.Closeable;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by lbsilva on 26-Nov-17.
+ */
+public abstract class FileTransmission {
+    
+    /**
+     * Server IP
+     */
+    protected final static String SERVER_IP = "REPLACE.WITH.SERVER.IP";
+
+    /**
+     * Server port
+     */
+    protected final static int SERVER_PORT = 6000;
+
+    /**
+     * Buffer size for file transfers
+     */
+    protected final static int BUFFER_SIZE = 1024 * 16;
+
+    /**
+     * File to be sent/received
+     */
+    protected File file;
+
+    /**
+     * Current status of file transfer
+     */
+    protected String status;
+
+    /**
+     * If the file transmission was canceled
+     */
+    protected boolean canceled;
+
+    /**
+     * The sslSocket to be used
+     */
+    protected SSLSocket sslSocket;
+
+    /**
+     * Helper for file transmission operations
+     * @param path - Path of the file to be sent/received
+     */
+    public FileTransmission (String path) {
+        if (path.isEmpty()) {
+            throw new IllegalArgumentException("Empty file path");
+        }
+        this.file = new File(path);
+        canceled = false;
+        try {
+            SSLSocketFactory sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+            sslSocket = (SSLSocket) sslSocketFactory.createSocket(SERVER_IP, SERVER_PORT);
+        } catch (IOException ex) {
+            throw new UnreachableHostException("Unreachable Host.");
+        }
+    }
+
+    /**
+     * Getter for status
+     * @return the status of the current transfer
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * Finishes a transformation process by closing all the streams
+     * @param closeables the streams
+     */
+    protected void finish(Closeable... closeables) {
+        for (Closeable closeable : closeables) {
+            try {
+                closeable.close();
+            } catch (Exception ignored) {
+            }
+        }
+    }
+
+    /**
+     * Starts the process
+     */
+    public abstract void start();
+}
